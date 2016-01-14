f3b3537f81b9e96baa946845aa01d67de8ed64b0
yuan232007 <yuan232007@163.com>
2016-01-14 22:05:25 +0800

    Initial commit

---
+++ app/src/main/java/merchant/hdw/com/hdw_merchant/MainActivity.java
@@ -0,0 +1,13 @@
+package merchant.hdw.com.hdw_merchant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
